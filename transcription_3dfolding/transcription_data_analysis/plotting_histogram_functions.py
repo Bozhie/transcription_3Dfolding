2730cb7d677c9606761979f3a0aaae4b47d1028c
Bozhie <imbozhie@gmail.com>
2022-11-15 13:44:09 -0800

    Adding ChIP-distance plots and CTCF within TAD

--- transcription_3dfolding/transcription_data_analysis/plotting_histogram_functions.py
+++ transcription_3dfolding/transcription_data_analysis/plotting_histogram_functions.py
@@ -150,6 +150,78 @@ def plot_distance_histogram(
     )
     if plot_title != None:
         ax.set_title(plot_title)
+        
+        
+def plot_count_histogram(
+    df, 
+    count_col,
+    agg_key='DE_status',
+    category_colors={"up": 'tab:red', 
+                     "down": 'tab:blue', 
+                     "nonsig": 'tab:gray'},
+    bins=50,
+    cumulative=False,
+    plot_title=None,
+    ax=None,
+    add_legend=True
+):
+    """
+    Plots distribution of counts across different bins in 
+    each category defined in df['agg_key'].
+    
+    Parameters:
+    -----------
+    df: pandas dataframe that has counts in 'count_col'
+        and the categories labeled in 'agg_key'.
+    count_col: the column in df with the counts for plotting
+    agg_key: column in df containing category labels
+    category_colors: the category label values in df[agg_key] mapped
+        to the colors for plotting.
+    bins: number of bins for histogram.
+    cumulative: True/False for whether the plot should build up cumulative
+        distribution on x-axis.
+    plot_title: title or this plot
+    ax: the axis for plotting.
+
+    Returns:
+    --------
+    plot
+    """
+    
+    df = df.copy().dropna(subset=[count_col])
+    
+    if ax == None:
+        ax = plt.subplot()
+        
+    val_range = (df[count_col].min(), df[count_col].max())
+    
+    for cat, col in category_colors.items():
+    
+        cat_ix = np.where(df[agg_key] == cat)
+
+        dist = df.iloc[cat_ix][count_col]
+        ax.hist(dist,
+                bins=bins,
+                density=True,
+                range=val_range,
+                cumulative=cumulative,
+                histtype='step',
+                lw=1.5,
+                label=cat, 
+                color=col)
+
+    if add_legend:
+        ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    if cumulative:
+        ylab = 'cumulative count'
+    else:
+        ylab = 'frequency'
+    ax.set(
+        xlabel = ' '.join(count_col.split('_')),
+        ylabel = ylab
+    )
+    if plot_title != None:
+        ax.set_title(plot_title)
 
         
 def group_features_by_region(
@@ -284,3 +356,39 @@ def distribution_features_by_region(
     
     if plot_title != None:
         ax.set_title(plot_title)
+
+        
+def assign_bin(df, value_col, bin_edges, bin_col_name):
+    """
+    Takes a dataframe and appends a column of bins to
+    categorize the values in df['value_col'].
+    
+    Parameters:
+    -----------
+    df: dataframe
+    value_col: values (numerical) to be sorted into bins
+    bin_edges: an array of the [min, max) for sorting every bin
+    bin_col_name: new column in df with bin
+    
+    Returns:
+    --------
+    df with df[bin_col_name] populated with the value of
+        the bottom edge of the bin (e.g. if value_col=3, 
+        bin_edges=[0, 5, 10], bin = 0)
+    """
+    
+    df_out = df.copy()
+    
+    prev = None
+    
+    for i in bin_edges:
+        if prev == None:
+            prev = i
+            continue
+
+        bin_ix = (df['count_ctcf_peaks'] >= prev) & (df['count_ctcf_peaks'] < i)
+        df_out.loc[bin_ix, 'count_ctcf_bin'] = prev
+
+        prev = i
+    
+    return df_out
